d0e8ef455a09f3420a66bfcc50f689d94fc01f04
Daniel Joudat <danieljoudat@gmail.com>
2022-05-23 15:03:51 +0300

    initial codebase
    Signed-off-by: Daniel Joudat <danieljoudat@gmail.com>

---
+++ entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+cd m3o-dart
+WORK_DIR=$(pwd)
+
+echo "dart-publisher-action-> Working directory ${WORK_DIR}"
+
+ls -al
+
+echo 'dart-publisher-action-> setting up pub-credentials.josn file ...'
+echo '${PUB_CREDENTIALS}' > "~/.config/dart/pub-credentials.json"
+
+echo 'dart-publisher-action-> publishing m3o-dart to pub.dev ...'
+
+if [ ! -f '~/.config/dart/pub-credentials.json' ]
+then
+        echo "missing credentials"
+        exit 1
+fi
+
+dart pub lish -f
+
+if [ $? -eq 0 ]
+then
+    echo "::set-output name=success::true"
+else
+    echo "::set-output name=success::false"
+fi
+
+echo 'dart-publisher-action-> delete pub-credentials.json'
+rm ~/.config/dart/pub-credentials.json
